272412a5f7610145f9f2f09c7375dc61ea31d6d2
betinho37 <jose.roberto240398@gmail.com>
2019-09-14 13:00:11 -0300

    Desenvolvendo

--- resources/views/publicacoes/categorias/video.blade.php
+++ resources/views/publicacoes/categorias/video.blade.php
@@ -4,26 +4,38 @@
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
-    <link href="https://unpkg.com/nanogallery2/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="https://unpkg.com/nanogallery2/dist/jquery.nanogallery2.min.js"></script>
 
 </head>
 <body>
 @include('componentes.navbar')
 
-<div ID="ngy2p" data-nanogallery2='{
+{{--<div ID="ngy2p" data-nanogallery2='{
         "itemsBaseURL": "http://nanogallery2.nanostudio.org/samples/",
         "thumbnailWidth": "400",
          "thumbnailHeight": "200",
         "thumbnailAlignment": "center"
       }'>
 
-    @foreach($publicacao as $key => $publicacoes)
-    <a href="{{asset('uploads/' . $publicacoes->arquivo)}}" data-ngthumb="" alt="{{'uploads/' . $publicacoes->arquivo}}" data-ngdesc="Video">{{$publicacoes->titulo}}</a>
+    <a href="{{asset('uploads/' . $publicacoes->arquivo)}}" data-ngthumb="{{asset('uploads/' . $publicacoes->arquivo)}}" alt="{{asset('uploads/' . $publicacoes->arquivo)}}" data-ngdesc="Video">{{$publicacoes->titulo}}</a>
     @endforeach
 
 
-</div>
+</div>--}}
+<hr class="mt-4 mb-5">
+    <section class="p-1">
+        <div class="row">
+            @foreach($publicacao as $key => $publicacoes)
+                <div class="col-md-6">
+                    <h4 class="pb-4">{{$publicacoes->titulo}}</h4>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="{{asset('uploads/' . $publicacoes->arquivo)}}" allowfullscreen></iframe>
+                    </div>
+                    <h3>{{$publicacoes->descricao}}</h3>
+                </div>
+            @endforeach
+        </div>
+    </section>
+
 
 </body>
 </html>
