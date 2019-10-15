bb20aa2ace406bbeae14c4558ff6e563d6b9fea1
betinho37 <jose.roberto240398@gmail.com>
2019-10-15 11:21:30 -0300

    Desenvolvendo

--- resources/views/publicacoes/categorias/documento.blade.php
+++ resources/views/publicacoes/categorias/documento.blade.php
@@ -1,2 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <link href="{{ asset('css/customize.css') }}" rel="stylesheet">
+    @include('componentes.navbar')
+</head>
 
-</script>
+<body>
+
+<div class="container marketing" style="padding:30px">
+
+    <div class="row">
+        @foreach($publicacao as $publicacoes)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <div class="caption">
+                            <a  href="{{asset('uploads/' . $publicacoes->arquivo)}}">
+                                <img src="{{asset('uploads/' . $publicacoes->capa)}}"  alt="{{'uploads/' . $publicacoes->arquivo}}"
+                                     class="img-responsive"/></a>
+                        </div>
+                    </div>
+                    <div class="text-item" >
+                        <p>Titulo: {{$publicacoes->titulo}}</p>
+                        <p>Descrição: {{$publicacoes->descricao}}</p>
+                        <p>
+                            <a href="{{asset('uploads/' . $publicacoes->pdf)}}" download="{{asset('uploads/' . $publicacoes->pdf)}}" class="btn btn-primary" role="button">Download</a>
+                    </div>
+                </div>
+
+        @endforeach
+    </div>
+
+{{ $publicacao->links() }}
+</body>
+
+</html>
--- resources/views/publicacoes/edit.blade.php
+++ resources/views/publicacoes/edit.blade.php
@@ -2,7 +2,11 @@
 @section('title', 'Museu Virtual')
 
 <link href="{{ asset('css/customize.css') }}" rel="stylesheet" type="text/css">
-
+<style rel="stylesheet" type="text/css">
+    .row { display: table; }
+    .thumbnail { float: left; width: 60%; }
+    .conteudo {  float: left; width: 60%; }
+</style>
 @section('content')
 
     <div id="div3">
@@ -36,9 +40,19 @@
                 <div class="form-group row">
                     <div class="col-md-5">
                         @if($publicacao->categoriaid == 1 )
+                            <div class="row">
+                            <div class="thumbnail">
+                                    <div class="caption">
+                                        <img src="{{asset('uploads/' . $publicacao->capa)}}"  alt="{{'uploads/' . $publicacao->arquivo}}"
+                                             class="img-responsive"/></a>
+                                    </div>
+                            </div>
+                                <div class="conteudo">
                             <iframe src="{{asset((isset($publicacao) && $publicacao->arquivo!='')?'uploads/'.$publicacao->arquivo:'')}}"
                                     width="300px" height="300px">
                             </iframe>
+                                </div>
+                            </div>
                         @elseif( $publicacao->categoriaid == 2 )
                             <img id="grande"
                                  src="{{asset((isset($publicacao) && $publicacao->arquivo!='')?'uploads/'.$publicacao->arquivo:'')}}"
@@ -75,21 +89,17 @@
                 </label><br>
             </div><p></p>
             <div align="left"><p></p>
-
                 <label>Posição da Imagem:
                     <input type="radio" name="posicaoimagem" value="0"
                            {{isset($publicacao->posicaoimagem) && $publicacao->posicaoimagem == 0 ? 'checked' : '' }}
                            required>Vertical
                 </label>
-
                 <label>
                     <input type="radio" name="posicaoimagem" value="1"
                            {{isset($publicacao->posicaoimagem) && $publicacao->posicaoimagem == 1 ? 'checked' : '' }}
                            required>Horizontal
                 </label><br>
-
             </div><p></p>
-
             <div align="Center" class="form-group">
                 {!!Form::submit('Salvar', ['class="btn btn-primary"'])!!}
                 <td><a href="{{@url('api/publicacao').'/destroy/'.$publicacao->id}}" class="btn btn-danger"
@@ -98,13 +108,11 @@
                 <a href="/api/publicacoes/controle" class="btn btn-primary">Voltar</a>
             </div>
     </div>
-
     @else
         <div class="form-group">
             <strong>Nome:</strong>
             {!!($publicacao->nome)!!}
         </div>
-
         <div class="form-group">
             <strong>Titulo da Imagem:</strong>
             {!!($publicacao->titulo)!!}
@@ -113,7 +121,6 @@
             <strong>Descrição:</strong>
             {!!( $publicacao->descricao)!!}
         </div>
-
         @if($publicacao->arquivo != null )
             <div class="form-group row">
                 <div class="col-md-5">
@@ -126,24 +133,10 @@
             </div><br/>
         @else
         @endif
-        <script type="text/javascript">
 
-            function PreviewImage() {
-                var oFReader = new FileReader();
-                oFReader.readAsDataURL(document.getElementById("arquivo").files[0]);
-
-                oFReader.onload = function (oFREvent) {
-                    document.getElementById("uploadPreview").src = oFREvent.target.result;
-                };
-            };
-
-        </script>
-
-        </div>
 
         <div align="Center" class="form-group">
             <a href="/publicacoes/controle" class="btn btn-primary">Voltar</a>
-        </div>
         </div>
     @endif
     {!! Form::close() !!}
